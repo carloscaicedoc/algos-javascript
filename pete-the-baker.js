579bd9bbf43e2b08f4833223c924958fba8b0010
Carlos Caicedo <calicheccmusica@gmail.com>
2022-05-10 14:50:09 -0700

    Looping & Comparison of Objects

--- pete-the-baker.js
+++ pete-the-baker.js
@@ -57,7 +57,6 @@ console.log(cakes(chocolateCake, availableChocolateCake));
 console.log(cakes(RedVelvet, availableRV));
 console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}));
 
-
 // Another approach: 
 
 const cakes2 = (recipe, available) => {
